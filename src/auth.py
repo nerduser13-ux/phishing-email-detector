a0982b93c1d4f0ebc1ab22d1d185387120f62d35
nerduser13-ux <nerduser13@gmail.com>
2026-05-26 21:53:15 +0545

    Raw email parsing tested and working - BeautifulSoup HTML parsing verified

--- src/auth.py
+++ src/auth.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, session, abort
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session
 from flask_login import login_user, logout_user, login_required, current_user
 from models import db, User
+
 import bcrypt
 import pyotp
 import qrcode
