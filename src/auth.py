bb35549cbdf06c8289356a49c6bb55f6951fac3f
nerduser13-ux <nerduser13@gmail.com>
2026-05-27 20:44:02 +0545

    added abort package in auth.py

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash, request, session
+from flask import Blueprint, render_template, redirect, url_for, flash, request, session, abort
 from flask_login import login_user, logout_user, login_required, current_user
 from models import db, User
 
